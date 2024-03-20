188b6f44f1fcd9ad0da54c6d365fbf0500d60ab9
dalwerJABAIT <dalwer.jabait@gmail.com>
2024-03-21 00:16:16 +0600

    first commit

---
+++ database/factories/OrderFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Order>
+ */
+class OrderFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $products = ['Mobile', 'Laptop', 'Watch', 'Mac Book'];
+        static $invoice = 20;
+
+        return [
+            'product_name' => $products[rand(0, 3)],
+            'currency' => 'BDT',
+            'amount' => rand(1500, 2000),
+            'invoice' => $invoice++,
+            'status' => 'Pending',
+        ];
+    }
+}
